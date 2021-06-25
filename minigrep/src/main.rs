a513bbb97ffcd03740b12606d693be92270a8208
Nic FitzGerald <nicholas.a.fitzgerald@gmail.com>
2021-06-25 13:36:04 -0500

    Starting on GREP in Rust

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,14 +1,22 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
 
-  let config = Config::new(&args);
+  let config = Config::new(&args).unwrap_or_else(|err| {
+      println!("Problem parsing arguments: {}", err);
+      process::exit(1);
+  });
 
   println!("Searching for {}\n", config.query);
   println!("In file {}\n", config.filename);
 
+  run(config);
+}
+
+fn run(config: Config) {
   let contents = fs::read_to_string(config.filename)
     .expect("Something went wrong reading the file.");
 
@@ -21,10 +29,14 @@ struct Config {
 }
 
 impl Config {
-  fn new(args: &[String]) -> Config {
+  fn new(args: &[String]) -> Result<Config, &str> {
+    if args.len() < 3 {
+      return Err("Not enough arguments");
+    }
+
     let query = args[1].clone();
     let filename = args[2].clone();
     
-    Config { query, filename }
+    Ok(Config { query, filename })
   }
 }
