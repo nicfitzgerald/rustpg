b455968f8522922316f490ad69b127d6cb5d4183
Nic FitzGerald <nicholas.a.fitzgerald@gmail.com>
2021-07-01 19:52:13 -0500

    restarting minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,42 +1,11 @@
 use std::env;
-use std::fs;
-use std::process;
 
 fn main() {
   let args: Vec<String> = env::args().collect();
+  
+  let query = &args[1];
+  let filename = &args[2];
 
-  let config = Config::new(&args).unwrap_or_else(|err| {
-      println!("Problem parsing arguments: {}", err);
-      process::exit(1);
-  });
-
-  println!("Searching for {}\n", config.query);
-  println!("In file {}\n", config.filename);
-
-  run(config);
-}
-
-fn run(config: Config) {
-  let contents = fs::read_to_string(config.filename)
-    .expect("Something went wrong reading the file.");
-
-  println!("With text:\n{}", contents);
-}
-
-struct Config {
-  query: String,
-  filename: String
-}
-
-impl Config {
-  fn new(args: &[String]) -> Result<Config, &str> {
-    if args.len() < 3 {
-      return Err("Not enough arguments");
-    }
-
-    let query = args[1].clone();
-    let filename = args[2].clone();
-    
-    Ok(Config { query, filename })
-  }
+  println!("Searching for {}", query);
+  println!("In file {}", filename); 
 }
